3033157ef6e23b5faeea224c3e60aac44ef81e05
maianh202 <23a4040005@hvnh.edu.vn>
2023-01-05 17:57:22 +0700

    update

--- baohanh.php
+++ baohanh.php
@@ -1,68 +1,112 @@
 <!DOCTYPE html>
-
-<?php
+<?php 
 require("header.php");
 ?>
 <html>
+    <main class="main">
+        <div class="page-content" >
+        <div class="container" style="font-family:roboto">
+            <div class="row">
+                <div class="col-12">
+                    <p style="margin: 15px 15px">Chào mừng Quý khách! <br> Dưới đây là chi tiết thông tin bảo hành các sản phẩm,<a href="index.php"> SunPhone</a> luôn nỗ lực để Quý khách hài lòng và tin tưởng khi mua hàng.
+                       <br> Chân thành cảm ơn và mong được phục vụ Quý khách!</p>
+                	<h2 style="margin: 15px 15px; text-align:center" >CHÍNH SÁCH BẢO HÀNH</h2><!-- End .title mb-3 -->
+                </div><!-- End .col-12 -->
+                		<div class="tabs-vertical">
+		            		<ul class="nav nav-tabs nav-tabs-bg flex-column" id="tabs-7" role="tablist">
+								<li class="nav-item">
+								    <a class="nav-link active" id="tab-25-tab" data-toggle="tab" href="#tab-25" role="tab" aria-controls="tab-25" aria-selected="true">Đổi mới 30 ngày miễn phí</a>
+								</li>
+								<li class="nav-item">
+								    <a class="nav-link" id="tab-26-tab" data-toggle="tab" href="#tab-26" role="tab" aria-controls="tab-26" aria-selected="false">Bảo hành tiêu chuẩn</a>
+								</li>
+							</ul>
+								<div class="tab-content tab-content-border" id="tab-content-7">
+								    <div class="tab-pane fade show active" id="tab-25" role="tabpanel" aria-labelledby="tab-25-tab">
+								    	<p>Khi mua sản phẩm tại Cellphones.com.vn, khách hàng có quyền hoàn toàn yên tâm với sản phẩm chính hãng, được bảo hành chính thức tại hãng và ngoài ra có thêm chính sách đổi mới miễn phí lên tới 30 ngày tại SunPhone</p>
+                                        <!-- <table class="table table-bordered">
+                                            <thead>
+                                                <tr class="success">
+                                                    <th style="text-align: center;"> <span style="font-size: small;">
+                                                        <strong>Loại sản phẩm</strong> </span>
+                                                    </th>
+                                                    <th style="text-align: center;"><span style="font-size: small;">
+                                                        <strong>Đổi mới miễn phí (*)</strong></span>
+                                                    </th>
+                                                    <th style="text-align: center;"><span style="font-size: small;">
+                                                        <strong>Quy định nhập lại, trả lại</strong></span>
+                                                    </th></tr>
+                                            </thead> 
+                                            <tbody>
+                                                <tr>
+                                                    <td style="text-align: left;"><span style="font-size: small;">Điện thoại</span></td> 
+                                                    
+                                                    <td style="text-align: center;"><span style="font-size: small;">30 ngày</span></td> 
+                                                    
+                                                    <td style="text-align: left;"><span style="font-size: small;">- Trong 30 ngày đầu nhập lại máy, trừ phí 20% trên giá hiện tại(hoặc giá mua nếu giá mua thấp hơn giá hiện tại)</span><br><span style="font-size: small;">- Sau 30 ngày nhập lại máy theo giá thoả thuận</span></td>    
+                                                </tr>
+                                                
+                                                <tr><td style="text-align: left;"><span style="font-size: small;">Bảo hành mở rộng</span></td> <td style="text-align: center;"><span style="font-size: small;">15 ngày</span></td> <td style="text-align: left;"><span style="font-size: small;">Trong 7 ngày đầu, máy không lỗi, khách hàng trả lại gói BHMR</span><br><span style="font-size: small;">&gt; Nhập lại trừ phí 50% gói BHMR</span></td></tr>
+                                            </tbody>
+                                        </table> -->
+
+                                        <table class="table table-striped table-bordered" >
+                                            <thead >
+                                                <tr style="text-align:center; background-color:tomato">
+                                                <th style="color:black; font-size:18px" >THÁNG 1</th>
+                                                <th style="color:black; font-size:18px">THÁNG 2-12</th>
+                                                </tr>
+                                            </thead>
+
+                                            <tbody>
+                                                <tr style="text-align:center">
+                                                <td colspan="2">Sản phẩm lỗi do nhà sản xuất</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td> &ensp;Miễn phí đổi sản phẩm tương đương: cùng model, cùng dung lượng, cùng thời gian bảo hành…<br>
+                                                &ensp;Trường hợp không có sản phẩm tương đương, FPT Shop hoàn lại tiền 100%
+                                                </td>
+                                                <td>&ensp;FPT Shop gửi máy đi bảo hành theo chính sách của hãng hoặc bảo hành của FPTShop.com.vn
+                                                </td>
+                                                </tr>
+
+                                                <tr style="text-align:center">
+                                                <td colspan="2">Sản phẩm không lỗi</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td colspan="2">Không áp dụng đổi trả</td>
+                                                </tr>
+
+                                                <tr style="text-align:center">
+                                                <td colspan="2">Sản phẩm lỗi do người sử dụng</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td colspan="2"><strong> &ensp;Không áp dụng đổi trả</strong> với sản phẩm:<br>
+                                                &ensp;- Máy không còn giữ nguyên 100% hình dạng ban đầu, bao gồm: có dấu hiệu va chạm mạnh, cấn móp, bị vào nước...<br>
+                                                &ensp;- Không đủ điều kiện bảo hành theo chính sách của hãng.<br>
+                                                &ensp;Trong trường hợp này, FPT Shop hỗ trợ chuyển TTBH và khách hàng chịu phí sửa chữa.
+                                                </td>
+                                                </tr>
+
+                                            </tbody>
+                                        </table>
+								    </div><!-- .End .tab-pane -->
+								    <div class="tab-pane fade" id="tab-26" role="tabpanel" aria-labelledby="tab-26-tab">
+								    	<p>Nobis perspiciatis natus cum, sint dolore earum rerum tempora aspernatur numquam velit tempore omnis, delectus repellat facere voluptatibus nemo non fugiat consequatur repellendus! Enim, commodi, veniam ipsa voluptates quis amet.</p>
+								    </div><!-- .End .tab-pane -->
+								</div><!-- End .tab-content -->
+						</div><!-- End .tabs-vertical -->
+            </div><!-- End .row -->
+        </div><!-- End .col-md-6 -->
+</div>
+</main>      
 
 
 
-
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<body>
-    <div class="header-bottom sticky-header">
-        <div class="row" style="margin-left: 0px">
-            <p>Chào mừng Quý khách !</p>
-            <p>Dưới đây là chi tiết thông tin bảo hành các sản phẩm, SunPhone luôn nỗ lực để Quý khách hài lòng và tin tưởng khi mua hàng</p>
-            <p>Chân thành cảm ơn và mong được phục vụ Quý khách!</p>
-        </div>
-        <div class="container row col-lg-12" style="border-top-style: solid">
-            <div class="w3-bar-block col-lg-3">
-                <h5 class="w3-bar-item"></h5>
-                <button class="w3-bar-item w3-button tablink" onclick="openTab(event, 'Csdt')">I.Đổi mới 30 ngày miễn phí</button>
-                <button class="w3-bar-item w3-button tablink" onclick="openTab(event, 'Csbh')">II.Bảo hành tiêu chuẩn</button>
-            </div>
-
-            <div class="col-lg-9" style="border-left-style: solid">
-
-                <div style="text-align: center">CHÍNH SÁCH BẢO HÀNH</div>
-
-                <div id="Csdt" class="w3-container policy" style="display:none">
-                    <h2>Chính sách đổi trả</h2>
-                    <p>Khi mua sản phẩm tại Cellphones.com.vn, khách hàng có quyền hoàn toàn yên tâm với sản phẩm chính hãng, 
-                        được bảo hành chính thức tại hãng và ngoài ra có thêm chính sách đổi mới miễn phí lên tới 30 ngày tại CellphoneS</p>
-                    <p>It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-                </div>
-
-                <div id="Csbh" class="w3-container policy" style="display:none">
-                    <h2>Chính sách bảo hành</h2>
-                    <p>Paris is the capital of France.</p> 
-                    <p>The Paris area is one of the largest population centers in Europe, with more than 12 million inhabitants.</p>
-                </div>
-
-                
-            </div>     
-        </div>
-    </div>
-
-<script>
-function openTab(evt, cityName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("policy");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " w3-red";
-}
-</script>
-
-</body>
 </html>
-<?php
+<?php 
 require("footer.php");
 ?>
